fb7e6e68a7021cb32b0e1a902f6678ab57fede8b
Daniel Kioko Nzioka <danielkioko.998@gmail.com>
2019-01-11 23:08:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase()){
-    return "I can't hear you!"
-  } else if (string.toUpperCase()){
-    return "YES INDEED!"
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
 }
+
+sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
